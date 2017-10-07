5b74d5f162e73940697fb3f2759c2ca790700cd2
wakeel <mahmoud.wakeel.eg@gmail.com>
2017-10-07 15:27:01 +0200

    admin main screen

--- app/src/main/java/com/linked_sys/tadreeb_ihssa/fragments/MainAdminFragment.java
+++ app/src/main/java/com/linked_sys/tadreeb_ihssa/fragments/MainAdminFragment.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,12 +37,10 @@ public class MainAdminFragment extends Fragment {
     MainActivity activity;
     Spinner periodsSpinner;
     ArrayAdapter<Periods> periodAdapter;
-    TextView txtPendingProgramsCount, txtAcceptedProgramsCount, txtNotAcceptedProgramsCount,
-            txtTeachersCount, txtTechTicketCount;
-    CardView btnPendingPrograms, btnSearch;
-    RelativeLayout btnTeachers, btnTechTicket, btnAcceptedPrograms, btnNotAcceptedPrograms;
-    LinearLayout btnApprovedPrograms, btnNotApprovedPrograms, btnSupportTicket, btnTeachersData;
     String period;
+    TextView pendingProgramsCountTxt;
+    LinearLayout teachersDataBtn, acceptedProgramsBtn, notAcceptedProgramsBtn, techTicketBtn, btnSearch;
+    RelativeLayout pendingProgramsBtn;
 
     @Nullable
     @Override
@@ -55,21 +52,37 @@ public class MainAdminFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         periodsSpinner = (Spinner) view.findViewById(R.id.periodsSpinner);
-        txtPendingProgramsCount = (TextView) view.findViewById(R.id.txtPendingProgramsCount);
-        txtAcceptedProgramsCount = (TextView) view.findViewById(R.id.txtAcceptedProgramsCount);
-        txtNotAcceptedProgramsCount = (TextView) view.findViewById(R.id.txtNotAcceptedProgramsCount);
-        txtTeachersCount = (TextView) view.findViewById(R.id.txtTeachersCount);
+        pendingProgramsCountTxt = (TextView) view.findViewById(R.id.pendingProgramsCountTxt);
+        teachersDataBtn = (LinearLayout) view.findViewById(R.id.preLoginLogo);
+        techTicketBtn = (LinearLayout) view.findViewById(R.id.btnAdminLoginContainer);
+        acceptedProgramsBtn = (LinearLayout) view.findViewById(R.id.btnStudentLoginContainer);
+        notAcceptedProgramsBtn = (LinearLayout) view.findViewById(R.id.btnTeacherLoginContainer);
+        btnSearch = (LinearLayout) view.findViewById(R.id.btnSearch);
+        pendingProgramsBtn = (RelativeLayout) view.findViewById(R.id.pendingProgramsBtn);
+        pendingProgramsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activity.openActivity(AdminPendingProgramsActivity.class);
+            }
+        });
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent acceptedIntent = new Intent(activity, SearchActivity.class);
+                acceptedIntent.putExtra("period", period);
+                startActivity(acceptedIntent);
+            }
+        });
+        periodsSpinner = (Spinner) view.findViewById(R.id.periodsSpinner);
         ImageView imgSpinner = (ImageView) view.findViewById(R.id.imgSpinner);
-        RelativeLayout spinnerBtn = (RelativeLayout) view.findViewById(R.id.spinnerBtn);
-        btnPendingPrograms = (CardView) view.findViewById(R.id.btnPendingPrograms);
-        btnAcceptedPrograms = (RelativeLayout) view.findViewById(R.id.btnAcceptedPrograms);
-        btnNotAcceptedPrograms = (RelativeLayout) view.findViewById(R.id.btnNotAcceptedPrograms);
-        btnSearch = (CardView) view.findViewById(R.id.btnSearch);
-        btnTeachers = (RelativeLayout) view.findViewById(R.id.btnTeachers);
-        btnTechTicket = (RelativeLayout) view.findViewById(R.id.btnTechTicket);
-        txtTechTicketCount = (TextView) view.findViewById(R.id.txtTechTicketCount);
+        imgSpinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                periodsSpinner.performClick();
+            }
+        });
 
-        view.findViewById(R.id.btnApprovedPrograms).setOnClickListener(new View.OnClickListener() {
+        acceptedProgramsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent acceptedIntent = new Intent(activity, AdminProgramsActivity.class);
@@ -77,7 +90,7 @@ public class MainAdminFragment extends Fragment {
                 startActivity(acceptedIntent);
             }
         });
-        view.findViewById(R.id.btnNotApprovedPrograms).setOnClickListener(new View.OnClickListener() {
+        notAcceptedProgramsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent acceptedIntent = new Intent(activity, AdminProgramsActivity.class);
@@ -85,13 +98,13 @@ public class MainAdminFragment extends Fragment {
                 startActivity(acceptedIntent);
             }
         });
-        view.findViewById(R.id.btnTeachersData).setOnClickListener(new View.OnClickListener() {
+        teachersDataBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 activity.openActivity(TeachersActivity.class);
             }
         });
-        view.findViewById(R.id.btnSupportTicket).setOnClickListener(new View.OnClickListener() {
+        techTicketBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 activity.openActivity(TechnicalTicketsActivity.class);
@@ -103,22 +116,12 @@ public class MainAdminFragment extends Fragment {
                 periodsSpinner.performClick();
             }
         });
-        spinnerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                periodsSpinner.performClick();
-            }
-        });
         getPeriods();
-        getTeachersCount();
-        getTechTicketsCount();
         periodsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Periods periods = (Periods) parent.getSelectedItem();
                 getPendingProgramsCount(periods.getPeriodREF());
-                getAcceptedProgramsCount(periods.getPeriodREF());
-                getNotAcceptedProgramsCount(periods.getPeriodREF());
                 CacheHelper.getInstance().adminPeriodSelectedID = periods.getPeriodREF();
                 period = periods.getPeriodName();
             }
@@ -128,50 +131,7 @@ public class MainAdminFragment extends Fragment {
 
             }
         });
-        btnPendingPrograms.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                activity.openActivity(AdminPendingProgramsActivity.class);
-            }
-        });
-        btnAcceptedPrograms.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent acceptedIntent = new Intent(activity, AdminProgramsActivity.class);
-                acceptedIntent.putExtra("status", "1");
-                startActivity(acceptedIntent);
-            }
-        });
-        btnNotAcceptedPrograms.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent acceptedIntent = new Intent(activity, AdminProgramsActivity.class);
-                acceptedIntent.putExtra("status", "2");
-                startActivity(acceptedIntent);
-            }
-        });
 
-        btnTeachers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                activity.openActivity(TeachersActivity.class);
-            }
-        });
-        btnTechTicket.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                activity.openActivity(TechnicalTicketsActivity.class);
-            }
-        });
-
-        btnSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent acceptedIntent = new Intent(activity, SearchActivity.class);
-                acceptedIntent.putExtra("period", period);
-                startActivity(acceptedIntent);
-            }
-        });
     }
 
     private void setPeriodsData() {
@@ -224,87 +184,7 @@ public class MainAdminFragment extends Fragment {
             @Override
             public void onSuccess(Object response) {
                 JSONObject res = (JSONObject) response;
-                txtPendingProgramsCount.setText(res.optString("con"));
-            }
-
-            @Override
-            public void onFailure(VolleyError error) {
-
-            }
-        });
-        api.executeRequest(true, false);
-    }
-
-    private void getAcceptedProgramsCount(String periodID) {
-        String url = ApiEndPoints.ADMIN_PROGRAMS_COUNT_URL
-                + "?APPCode=" + CacheHelper.getInstance().appCode
-                + "&UserId=" + activity.session.getUserDetails().get(activity.session.KEY_NATIONAL_ID)
-                + "&PeriodId=" + periodID
-                + "&ProgStatus=1";
-        ApiHelper api = new ApiHelper(activity, url, Request.Method.GET, new ApiCallback() {
-            @Override
-            public void onSuccess(Object response) {
-                JSONObject res = (JSONObject) response;
-                txtAcceptedProgramsCount.setText(res.optString("con"));
-            }
-
-            @Override
-            public void onFailure(VolleyError error) {
-
-            }
-        });
-        api.executeRequest(true, false);
-    }
-
-    private void getNotAcceptedProgramsCount(String periodID) {
-        String url = ApiEndPoints.ADMIN_PROGRAMS_COUNT_URL
-                + "?APPCode=" + CacheHelper.getInstance().appCode
-                + "&UserId=" + activity.session.getUserDetails().get(activity.session.KEY_NATIONAL_ID)
-                + "&PeriodId=" + periodID
-                + "&ProgStatus=2";
-        ApiHelper api = new ApiHelper(activity, url, Request.Method.GET, new ApiCallback() {
-            @Override
-            public void onSuccess(Object response) {
-                JSONObject res = (JSONObject) response;
-                txtNotAcceptedProgramsCount.setText(res.optString("con"));
-            }
-
-            @Override
-            public void onFailure(VolleyError error) {
-
-            }
-        });
-        api.executeRequest(true, false);
-    }
-
-    private void getTeachersCount() {
-        String url = ApiEndPoints.TEACHERS_COUNT
-                + "?APPCode=" + CacheHelper.getInstance().appCode
-                + "&UserId=" + activity.session.getUserDetails().get(activity.session.KEY_NATIONAL_ID);
-        ApiHelper api = new ApiHelper(activity, url, Request.Method.GET, new ApiCallback() {
-            @Override
-            public void onSuccess(Object response) {
-                JSONObject res = (JSONObject) response;
-                txtTeachersCount.setText(res.optString("con"));
-            }
-
-            @Override
-            public void onFailure(VolleyError error) {
-
-            }
-        });
-        api.executeRequest(true, false);
-    }
-
-    private void getTechTicketsCount() {
-        String url = ApiEndPoints.ADMIN_TECH_TICKET_COUNT
-                + "?APPCode=" + CacheHelper.getInstance().appCode
-                + "&SchoolId=" + activity.session.getUserDetails().get(activity.session.KEY_ID);
-        ApiHelper api = new ApiHelper(activity, url, Request.Method.GET, new ApiCallback() {
-            @Override
-            public void onSuccess(Object response) {
-                JSONObject res = (JSONObject) response;
-                txtTechTicketCount.setText(res.optString("con"));
+                pendingProgramsCountTxt.setText(res.optString("con"));
             }
 
             @Override
@@ -319,7 +199,5 @@ public class MainAdminFragment extends Fragment {
     public void onResume() {
         super.onResume();
         getPeriods();
-        getTeachersCount();
-        getTechTicketsCount();
     }
 }
